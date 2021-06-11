d6df74383c7be2d8d4c13f155d8ed92d256a0511
Egor Matsegora <yegor.matsegora@gmail.com>
2021-06-11 09:54:31 +0300

    fix typing bug in rovers form

--- src/views/Rovers/components/RoversForm/RoversForm.vue
+++ src/views/Rovers/components/RoversForm/RoversForm.vue
@@ -4,29 +4,37 @@
 <script lang="ts">
 import Vue from 'vue';
 import { mapActions, mapState } from 'vuex';
+import { required } from 'vuelidate/lib/validators';
 import { v4 as uuid } from 'uuid';
 
 import Loader from '@/components/Loader/Loader.vue';
 import InputField from '@/components/InputField/InputField.vue';
+import UiButton from '@/components/UiButton/UiButton.vue';
 import { TRootState } from '@/store/types';
 import { TRoversState } from '@/store/modules/rovers/types';
 import { IRoverManifest } from '../../interfaces/rover-manifest';
 import { roverMap } from '../../rover-maps';
+import { IRoverFormData } from '../../interfaces/rover-form-data';
 
 export default Vue.extend({
   name: 'RoversForm',
-  components: { Loader, InputField },
-  data() {
-    return {
-      date: '',
-      camera: '',
-      id: uuid(),
-    };
+  components: { Loader, InputField, UiButton },
+  data: () => ({
+    date: '',
+    camera: '',
+    id: uuid(),
+  }),
+  validations: {
+    date: { required },
+    camera: { required },
   },
   computed: {
     ...mapState({
       loading(state: TRootState): boolean {
         return (state.rovers as TRoversState).manifestLoading;
+      },
+      photosLoading(state: TRootState): boolean {
+        return (state.rovers as TRoversState).imagesLoading;
       },
       roverManifest(state: TRootState): IRoverManifest | null {
         return (state.rovers as TRoversState).roverManifest;
@@ -35,23 +43,33 @@ export default Vue.extend({
         return (state.rovers as TRoversState).activeRover;
       },
     }),
-    roverCameras() {
+    roverCameras(): string[] | [] {
       const rover = roverMap.find((rover) => rover.name === this.activeRover);
       return rover ? rover.cameras : [];
     },
   },
   methods: {
-    handleSubmit: () => {
-      console.log('handleSubmit');
+    handleSubmit(): void {
+      this.$v.$touch();
+      if (this.$v.$invalid) return;
+      const reqData: IRoverFormData = {
+        date: this.date,
+        camera: this.camera,
+        roverName: this.activeRover,
+      };
+      this.fetchPhotos(reqData);
     },
-    ...mapActions(['fetchRoverManifestWithStorage']),
+    ...mapActions({
+      fetchManifest: 'fetchRoverManifestWithStorage',
+      fetchPhotos: 'fetchRoverPhotos',
+    }),
   },
   beforeRouteUpdate(to, from, next) {
-    this.fetchRoverManifestWithStorage();
+    this.fetchManifest();
     next();
   },
   beforeMount() {
-    this.fetchRoverManifestWithStorage();
+    this.fetchManifest();
   },
   filters: {
     dateToString(value: string): string {

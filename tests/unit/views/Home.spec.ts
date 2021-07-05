385ceeb3b2d9b238b13097b07f37eee119f5b7b6
Egor Matsegora <yegor.matsegora@gmail.com>
2021-07-05 11:48:59 +0300

    adding tests for home view in progress

---
+++ tests/unit/views/Home.spec.ts
@@ -0,0 +1,55 @@
+import { shallowMount, Wrapper, createLocalVue } from '@vue/test-utils';
+import Router from 'vue-router';
+import Vuex, { Store } from 'vuex';
+import { cloneDeep } from 'lodash';
+
+import { routes } from '@/router';
+import { home } from '@/store/modules/home';
+import Home from '@/views/Home/Home.vue';
+
+describe('HomeView', () => {
+  let wrapper: Wrapper<Home>;
+  let mockRouter: Router | null;
+  let mockStore: Store<any> | null;
+
+  const createComponent = () => {
+    const localVue = createLocalVue();
+
+    localVue.use(Router);
+    mockRouter = new Router({ routes: cloneDeep(routes) });
+
+    localVue.use(Vuex);
+    mockStore = new Vuex.Store({
+      modules: {
+        home: {
+          actions: {
+            fetchMainSliderDataWithStorage: jest.fn(),
+            fetchMainSliderData: jest.fn(),
+          },
+          state: {
+            mainSliderData: [],
+            mainSliderLoading: false,
+          },
+        },
+      },
+    });
+
+    wrapper = shallowMount(Home, {
+      localVue,
+      router: mockRouter,
+      store: mockStore,
+    });
+  };
+
+  beforeEach(() => createComponent());
+
+  it('[Home] should create', () => {
+    expect(wrapper.exists()).toBeTruthy();
+  });
+
+  afterEach(() => {
+    wrapper.destroy();
+    mockRouter = null;
+    mockStore = null;
+  });
+});

8078ac4aa82f1d069451640fe01a74c53b8aac27
Egor Matsegora <yegor.matsegora@gmail.com>
2021-07-27 17:20:03 +0300

    try add tests for rovers content

---
+++ tests/unit/views/RoversContent.spec.ts
@@ -0,0 +1,51 @@
+// import Vue from 'vue';
+// import Vuex, { Store } from 'vuex';
+// import RoversContent from '@/views/Rovers/components/RoversContent/RoversContent.vue';
+// import { Wrapper, createLocalVue, shallowMount } from '@vue/test-utils';
+// import { VueConstructor } from 'vue/types/umd';
+// import Loader from '@/components/Loader/Loader.vue';
+
+describe('RoversContent view', () => {
+  it.todo('VueEasyLightbox ломает все тесты');
+  // let wrapper: Wrapper<Vue>;
+  // let mockStore: Store<any> | null;
+  // let localVue: VueConstructor<any> | null;
+  // const photoUrls = jest.fn(() => ['test-url1', 'test-url2', 'test-url3']);
+  // const VueEasyLightboxMock = Vue.extend({
+  //   name: 'VueEasyLightbox',
+  //   props: ['visible', 'imgs', 'index', 'loop'],
+  // });
+  // const createComponent = () => {
+  //   localVue = createLocalVue();
+  //   localVue.use(Vuex);
+  //   mockStore = new Vuex.Store({
+  //     modules: {
+  //       rovers: {
+  //         state: {
+  //           imagesLoading: false,
+  //           roverPhotos: [],
+  //         },
+  //         getters: { photoUrls },
+  //       },
+  //     },
+  //   });
+  //   wrapper = shallowMount(RoversContent, {
+  //     localVue,
+  //     store: mockStore,
+  //     stubs: {
+  //       ['Loader']: Loader,
+  //       ['VueEasyLightbox']: VueEasyLightboxMock,
+  //     },
+  //   });
+  // };
+  // beforeEach(() => createComponent());
+  // it('[RoversContent] should create', () => {
+  //   console.log(wrapper.html());
+  //   expect(wrapper.exists()).toBeTruthy();
+  // });
+  // afterEach(() => {
+  //   wrapper.destroy();
+  //   mockStore = null;
+  //   localVue = null;
+  // });
+});
